b4e971429f218083eaa87f6cbe1ed2a1453580df
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-05-05 16:24:32 +0400

    merged(merged added feature and fix some errors)

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -6,6 +6,12 @@ import { SDiv, SGradient } from './Carousel.styled'
 import 'swiper/css'
 import 'swiper/css/navigation'
 
+const carousel_1 = require('../../assets/images/carousel_1.jpg')
+const carousel_2 = require('../../assets/images/carousel_2.jpg')
+const carousel_3_vid = require('../../assets/images/carousel_vid.mp4')
+const carousel_4 = require('../../assets/images/carousel_4.jpg')
+const carousel_5 = require('../../assets/images/carousel_5.jpg')
+
 export const Carousel = () => {
     return (
         <SDiv>
@@ -20,24 +26,21 @@ export const Carousel = () => {
                 navigation={true}
             >
                 <SwiperSlide>
-                    <img src='/images/carousel_1.jpg' alt='carousel_1' />
+                    <img src={carousel_1} alt='carousel_1' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src='/images/carousel_2.jpg' alt='carousel_2' />
+                    <img src={carousel_2} alt='carousel_2' />
                 </SwiperSlide>
                 <SwiperSlide className='bg-black'>
                     <video controls muted>
-                        <source
-                            src='/images/carousel_vid.mp4'
-                            type='video/mp4'
-                        />
+                        <source src={carousel_3_vid} type='video/mp4' />
                     </video>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src='/images/carousel_4.jpg' alt='carousel_4' />
+                    <img src={carousel_4} alt='carousel_4' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src='/images/carousel_5.jpg' alt='carousel_5' />
+                    <img src={carousel_5} alt='carousel_5' />
                 </SwiperSlide>
             </Swiper>
             <SGradient />
